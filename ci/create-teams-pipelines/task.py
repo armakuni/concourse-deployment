85b82fb2da3ad5bf2ca5f46bbb7aa36765dfe575
Fabio Ferrante <fabioferrante@Fabios-MacBook-Pro.local>
2019-09-11 14:08:44 +0100

    updated task.py

--- ci/create-teams-pipelines/task.py
+++ ci/create-teams-pipelines/task.py
@@ -71,7 +71,7 @@ def set_pipeline(path, target, pipeline_name, pipeline_config_path, pipeline_var
   Uuid = subprocess.check_output(("jq", ".uuid"), stdin=ps.stdout)
   ps.wait()
   
-  os.system("op get document " + Uuid.decode('ascii') + " &2>1 > gitkey.key")
+  os.system("op get document " + Uuid.decode('ascii') + " 2>&1 > gitkey.key")
 
   print("FILE GITKEY CONTENT:")
   os.system("cat gitkey.key")
